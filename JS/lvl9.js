31e56d7adee5fccff4f2556b734862e0520f7ff6
Yesica Picarelli <souprismatienda@gmail.com>
2025-10-08 11:21:47 -0300

    Agregando la clase del 8 de oct

---
+++ JS/lvl9.js
@@ -0,0 +1,5 @@
+function cambiarCosas (){
+    document.getElementById("demo4").innerHTML= "Fui cambiado!";
+    document.getElementById("demo4").style.color= "red";
+    document.getElementById("demo4").style.fontSize= "30px";
+}
