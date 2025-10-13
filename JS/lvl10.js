52cb878446acdf5332a64afb02fd6c7364717cb8
Yesica Picarelli <souprismatienda@gmail.com>
2025-10-13 10:58:25 -0300

    Agregando clase del 13 de oct

---
+++ JS/lvl10.js
@@ -0,0 +1,20 @@
+function sintaxis1(){
+    let x = 5;
+    const nombre = "Tinelli";
+
+    alert(x);
+    alert(nombre);
+}
+
+function operadorAsignacion(){
+    let x = 10;
+    let y = 32;
+    let suma = x + y;
+    console.log("El sentido de la vida, el universo y todo lo demas es:");
+    console.log(suma);
+}
+
+function operadoresAritmeticos (){
+    let operacion = (10 + 20) * 2 - 58 / 4;
+    document.getElementById("opes").innerText = operacion;
+}
